582bb7e522d8b3bd8bb42c429a4e77a5e31999d3
juane <juane.elizondo@yahoo.com>
2024-03-28 18:32:32 -0300

    cambie logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -53,8 +53,8 @@ const Navbar = () => {
             <div className="relative flex  items-center justify-between">
               
               <div className='w-full'>
-                <div className="flex justify-between items-center bg-[#0830A6] py-2 px-10">
-                  <a href='/' className=" sm:flex z-30 duration-300 cursor-pointer py-2 px-4 hover:animate-jiggle  font-bold"><img src="https://res.cloudinary.com/dfschbyq2/image/upload/v1711645269/Dise%C3%B1o_sin_t%C3%ADtulo__32_-removebg-preview_upscayl_4x_realesrgan-x4plus_lum9qe.png" alt="" className='w-48'/></a>
+                <div className="flex justify-between items-center bg-[#0830A6] py-3 px-10">
+                  <a href='/' className=" sm:flex z-30 duration-300 cursor-pointer py-2 px-4 hover:animate-jiggle  font-bold"><img src="https://res.cloudinary.com/dfschbyq2/image/upload/v1711661519/Dise%C3%B1o_sin_t%C3%ADtulo__34_-removebg-preview_upscayl_4x_realesrgan-x4plus_ppihnw.png" alt="" className='w-48'/></a>
                   <div className='hidden lg:flex lg:justify-center lg:items-center gap-10  rounded-md z-50 py-4'>
                       {navigation.map((item) => (
                         <a
